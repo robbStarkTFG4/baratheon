f7dab4c3c09ddf88446882a23572278145956371
cristianivn <cristianivn7@gmail.com>
2014-04-16 20:59:51 -0700

    Funciones de administrador
    Aqui estan las funciones del administrador,
    Agregar,Eliminar,Modificar,Reportes, hay una detalle el menu de
    administador se encima un poco al del index. para verlo tiene que
    cambiar el data source por el suyo..

--- webApp/src/java/com/server/beans/staless/TblPrestariosFacade.java
+++ webApp/src/java/com/server/beans/staless/TblPrestariosFacade.java
@@ -6,6 +6,8 @@
 package com.server.beans.staless;
 
 import com.server.entity.beans.TblPrestarios;
+import com.server.entity.beans.TblTipoprestarios;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -43,5 +45,183 @@ public class TblPrestariosFacade extends AbstractFacade<TblPrestarios> {
         }
       
     }
+     public int validar (String matricula){
+   try{     
+    System.out.println("METODO VALIDAR-FACADE");   
+    Query search= em.createQuery("SELECT t.idPrestario FROM TblPrestarios t WHERE t.usuario = :usuario").setParameter("usuario", matricula);
+    //search.setParameter("usuario", matricula);
+    int obj=(int)search.getSingleResult();
+    System.out.println(obj);
+  } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        }
+    return 0;
+    }
+    
+    
+    public boolean agregar (String nombre,String apaterno,String amaterno,int tipo,String tel, String email, String usuario,String carrera){
+   
+ boolean existe = true;
+       TblPrestarios id=null;   
+   Query search = em.createQuery("SELECT t FROM TblPrestarios t WHERE t.usuario = :usuario OR t.email = :correo");
+        search.setParameter("usuario",usuario);
+        search.setParameter("correo", email);
+   try{
+        id=(TblPrestarios)search.getSingleResult();
+   
+   
+   } catch (Exception e) {
+    TblPrestarios pres = new TblPrestarios();
+    pres.setNombre(nombre);
+    pres.setApaterno(apaterno);
+    pres.setAmaterno(amaterno);
+    pres.setTel(tel);
+    pres.setCarrera(carrera);
+    pres.setEmail(email);
+    pres.setUsuario(usuario);
+    TblTipoprestarios tp =new TblTipoprestarios();
+    tp.setIdTipoprestarios(tipo);      
+    pres.setIdTipoprestarios(tp);
+    getEntityManager().persist(pres);
+    existe=false;
+   System.out.println("Usuario creado");  
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        }
+        return existe;
 
+    
+
+    }
+    public boolean eliminar (String matricula){
+     try{
+              
+           Query search= em.createQuery("SELECT t FROM TblPrestarios t WHERE t.usuario = :usuario");
+          search.setParameter("usuario", matricula);  
+            TblPrestarios pres =(TblPrestarios)search.getSingleResult();
+          em.remove(pres);
+          System.out.println("Prestario con matricula: "+matricula+" ELIMINADO");
+          return true;
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+             return false;
+        }
+     
+    }
+  public List usuarios(){
+     
+  List<String> list = null ;    
+     
+     try{
+              
+           Query search= em.createQuery("SELECT t.usuario FROM TblPrestarios t");
+            list=search.getResultList();
+          System.out.println("LISTA DE prestarios");
+          System.out.println(list);
+         
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+            
+        }
+         
+     
+     
+     
+     return list;
+}    
+  public List prestarios(String patron){
+     
+  List<String> list = null ;    
+     
+     try{
+              
+           Query search= em.createQuery("SELECT t.usuario FROM TblPrestarios t WHERE t.usuario LIKE :patron");
+           search.setParameter("patron", patron.toLowerCase()+"%"); 
+            list=search.getResultList();
+          System.out.println("LISTA DE USUARIOS");
+          System.out.println(list);
+         
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+            
+        }
+         
+     
+     
+     
+     return list;
+}  
+  public TblPrestarios consultamodif(String usuario) {
+    
+        TblPrestarios user = null;
+        
+        try {
+        //TypedQuery<TblUsuarios> search =em.createQuery("SELECT NEW entity.TblUsuarios(u.idTipousuarios,u.usuario) FROM TblUsuarios u WHERE u.usuario=:usuario and u.contraseña=:clave",TblUsuarios.class);
+        Query search = em.createQuery("SELECT u FROM TblPrestarios u WHERE u.usuario=:usuario" );
+        search.setParameter("usuario", usuario);
+        
+            user = (TblPrestarios) search.getSingleResult();
+            System.out.println("usuario encontrado");
+        } catch (Exception e) {
+            return user;//System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        } finally {
+            return user;
+        }
+   
+        
+}   
+   public boolean modificar (int id,String nombre,String apaterno,String amaterno,int tipo,String tel, String email, String usuario,String carrera){
+   
+ boolean existe = true;
+       TblPrestarios bp=null;   
+   Query search = em.createQuery("SELECT t FROM TblPrestarios t WHERE t.email = :correo");
+        
+        search.setParameter("correo", email);
+   try{
+        bp=(TblPrestarios)search.getSingleResult();
+   if(usuario.equals(bp.getUsuario())){
+   TblPrestarios pres= em.find(TblPrestarios.class, id);
+    pres.setNombre(nombre);
+    pres.setApaterno(apaterno);
+    pres.setAmaterno(amaterno);
+    pres.setTel(tel);
+    pres.setCarrera(carrera);
+    pres.setEmail(email);
+    pres.setUsuario(usuario);
+    TblTipoprestarios tp =new TblTipoprestarios();
+    tp.setIdTipoprestarios(tipo);      
+    pres.setIdTipoprestarios(tp);
+    getEntityManager().merge(pres);
+    existe=false;
+   System.out.println("usuario modificado"); 
+   
+   }
+   else{
+       existe=true;
+   }
+   } catch (Exception e) {
+     TblPrestarios pres= em.find(TblPrestarios.class, id);
+    pres.setNombre(nombre);
+    pres.setApaterno(apaterno);
+    pres.setAmaterno(amaterno);
+    pres.setTel(tel);
+    pres.setCarrera(carrera);
+    pres.setEmail(email);
+    pres.setUsuario(usuario);
+    TblTipoprestarios tp =new TblTipoprestarios();
+    tp.setIdTipoprestarios(tipo);      
+    pres.setIdTipoprestarios(tp);
+    getEntityManager().merge(pres);
+    existe=false;
+   System.out.println("Usuario modificado");  
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        }
+   
+   
+        return existe;
+
+    
+
+    }
 }
+
+
--- webApp/src/java/com/server/beans/staless/TblUsuariosFacade.java
+++ webApp/src/java/com/server/beans/staless/TblUsuariosFacade.java
@@ -6,10 +6,13 @@
 
 package com.server.beans.staless;
 
+import com.server.entity.beans.TblTipousuarios;
 import com.server.entity.beans.TblUsuarios;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +31,189 @@ public class TblUsuariosFacade extends AbstractFacade<TblUsuarios> {
     public TblUsuariosFacade() {
         super(TblUsuarios.class);
     }
+   public TblUsuarios logIn(String usuario, String clave) {
     
+        TblUsuarios user = null;
+        
+        try {
+        //TypedQuery<TblUsuarios> search =em.createQuery("SELECT NEW entity.TblUsuarios(u.idTipousuarios,u.usuario) FROM TblUsuarios u WHERE u.usuario=:usuario and u.contraseña=:clave",TblUsuarios.class);
+        Query search = em.createQuery("SELECT u FROM TblUsuarios u WHERE u.usuario=:usuario and u.contraseña=:clave");
+        search.setParameter("usuario", usuario);
+        search.setParameter("clave", clave);
+            user = (TblUsuarios) search.getSingleResult();
+        } catch (Exception e) {
+            return user;//System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        } finally {
+            return user;
+        }
+   
+        
+} 
+   public boolean agregar (String nombre,String clave,String apellidop,String apellidom, String tipous, String email, String tel,String usuario){ 
+    boolean existe = true;
+       TblUsuarios id=null;   
+   Query search = em.createQuery("SELECT t FROM TblUsuarios t WHERE t.usuario = :usuario OR t.email = :correo");
+        search.setParameter("usuario",usuario);
+        search.setParameter("correo", email);
+   try{
+        id=(TblUsuarios)search.getSingleResult();
+   
+   
+   } catch (Exception e) {
+   TblUsuarios us=new TblUsuarios();
+   TblTipousuarios tu=new TblTipousuarios();
+   us.setNombre(nombre);
+   us.setContraseña(clave);
+   us.setApellidop(apellidop);
+   us.setApellidom(apellidom);
+   tu.setIdTipousuarios(tipous);
+   us.setIdTipousuarios(tu);
+   us.setEmail(email);
+   us.setTel(tel);
+   us.setUsuario(usuario);
+    getEntityManager().persist(us);
+    existe=false;
+   System.out.println("Usuario creado");  
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        }
+        return existe;
+   }
+   
+   public boolean eliminar (String usuario){
+       
+          
+          try{
+              
+           Query search= em.createQuery("SELECT t FROM TblUsuarios t WHERE t.usuario = :usuario");
+          search.setParameter("usuario", usuario);  
+            TblUsuarios pres =(TblUsuarios)search.getSingleResult();
+          em.remove(pres);
+          System.out.println("Prestario con matricula: "+usuario+" ELIMINADO");
+          return true;
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+             return false;
+        }
+        
+  
+   
+    }
+   
+   
+ public List usuarios(String patron){
+     
+  List<String> list = null ;    
+     
+     try{
+              
+           Query search= em.createQuery("SELECT t.usuario FROM TblUsuarios t WHERE t.usuario LIKE :patron");
+           search.setParameter("patron", patron.toLowerCase()+"%"); 
+            list=search.getResultList();
+          System.out.println("LISTA DE USUARIOS");
+          System.out.println(list);
+         
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+            
+        }
+         
+     
+     
+     
+     return list;
+}  
+   
+ public TblUsuarios consultamodif(String usuario) {
+    
+        TblUsuarios user = null;
+        
+        try {
+        //TypedQuery<TblUsuarios> search =em.createQuery("SELECT NEW entity.TblUsuarios(u.idTipousuarios,u.usuario) FROM TblUsuarios u WHERE u.usuario=:usuario and u.contraseña=:clave",TblUsuarios.class);
+        Query search = em.createQuery("SELECT u FROM TblUsuarios u WHERE u.usuario=:usuario" );
+        search.setParameter("usuario", usuario);
+        
+            user = (TblUsuarios) search.getSingleResult();
+            System.out.println("usuario encontrado");
+        } catch (Exception e) {
+            return user;//System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        } finally {
+            return user;
+        }
+   
+        
+}    
+  public List listausuariosSS (){
+     List< TblUsuarios> lista=null;
+      String usuario="[entity.TblTipousuarios[ idTipousuarios=uss ]]";
+   try{
+              
+           Query search= em.createQuery("SELECT u FROM TblUsuarios u WHERE u.idTipousuarios=:usuario" );
+            search.setParameter("usuario", usuario);
+        
+            lista=search.getResultList();
+          System.out.println("usuarios del servicio social");
+          System.out.println(lista);
+
+         
+        } catch (Exception e) {
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+            
+        }
+  
+  return lista;
+  
+  
+  } 
+     public boolean modificar (int id,String nombre,String clave,String apellidop,String apellidom, String tipous, String email, String tel,String usuario){ 
+    boolean existe = true;
+    
+     TblUsuarios busc;  
+     Query search = em.createQuery("SELECT t FROM TblUsuarios t WHERE t.email = :correo");
+       
+        search.setParameter("correo", email);
+   try{
+        busc=(TblUsuarios)search.getSingleResult();
+        if(usuario.equals(busc.getUsuario())){
+        TblUsuarios us= em.find(TblUsuarios.class, id);
+   TblTipousuarios tu=new TblTipousuarios();
+   us.setNombre(nombre);
+   us.setContraseña(clave);
+   us.setApellidop(apellidop);
+   us.setApellidom(apellidom);
+   tu.setIdTipousuarios(tipous);
+   us.setIdTipousuarios(tu);
+   us.setEmail(email);
+   us.setTel(tel);
+   us.setUsuario(usuario);
+    getEntityManager().persist(us);
+   
+   System.out.println("Usuario creado");  
+   existe=false;
+  
+        }
+        else{
+        existe=true;
+        }
+   
+   
+   } catch (Exception e) {
+    TblUsuarios us= em.find(TblUsuarios.class, id);
+   TblTipousuarios tu=new TblTipousuarios();
+   us.setNombre(nombre);
+   us.setContraseña(clave);
+   us.setApellidop(apellidop);
+   us.setApellidom(apellidom);
+   tu.setIdTipousuarios(tipous);
+   us.setIdTipousuarios(tu);
+   us.setEmail(email);
+   us.setTel(tel);
+   us.setUsuario(usuario);
+    getEntityManager().persist(us);
+    existe=false;
+   System.out.println("Usuario creado");  
+            System.out.println("ERROR IN Question FACADE:" + e.getMessage());
+        }
+        return existe;
+   }
+   
 }
